88fdf764202f1aae99cd31604106d00874ebc0cc
OnytSukaBatu <valentynoelsan@gmail.com>
2025-08-23 20:32:40 +0700

    [0.1.5] Main Color

---
+++ lib/core/function/main_color.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static const MaterialColor primary = MaterialColor(0xFF4040FF, {
+    50: Color(0xFFE0E0FF),
+    100: Color(0xFFC0C0FF),
+    200: Color(0xFFA0A0FF),
+    300: Color(0xFF8080FF),
+    400: Color(0xFF6060FF),
+    500: Color(0xFF4040FF),
+    600: Color(0xFF2020FF),
+    700: Color(0xFF0000FF),
+    800: Color(0xFF0000E8),
+    900: Color(0xFF0000D3),
+  });
+
+  static const Color blue = Color(0xFF2196F3);
+  static const Color deepPurple = Color(0xFF673AB7);
+  static const Color teal = Color(0xFF009688);
+  static const Color red = Color(0xFFF44336);
+  static const Color green = Color(0xFF4CAF50);
+  static const Color amber = Color(0xFFFFC107);
+  static const Color cyan = Color(0xFF00BCD4);
+  static const Color orange = Color(0xFFFF9800);
+  static const Color pink = Color(0xFFE91E63);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color lightBlueGrey = Color(0xFFECEFF1);
+  static const Color black = Color(0xFF000000);
+  static const Color transparant = Color(0x00000000);
+}
--- lib/theme_example.dart
+++ lib/theme_example.dart
@@ -1,152 +1,324 @@
+import 'package:core/core/function/main_color.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-ThemeData themeExample = ThemeData(
-  brightness: Brightness.light,
+class MainTheme {
+  // static Color primaryColor = Color(0xFF0080FF);
+  // static Color primaryLighter = Color(0xFF8080FF);
 
-  // Text, RichText, TextSpan
-  textTheme: TextTheme(
-    bodyLarge: GoogleFonts.poppins(color: Color(0xFF000000)),
-    bodyMedium: GoogleFonts.poppins(color: Color(0xFF000000)),
-    bodySmall: GoogleFonts.poppins(color: Color(0xFF000000)),
-    titleLarge: GoogleFonts.poppins(color: Color(0xFF000000)),
-  ),
+  ThemeData light = ThemeData(
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: Color(0xFFFFFFFF),
 
-  // TextFormField, DropDownButton
-  inputDecorationTheme: InputDecorationTheme(
-    hintStyle: GoogleFonts.poppins(color: Color(0xFF404040)),
-    labelStyle: GoogleFonts.poppins(color: Color(0xFF000000)),
-    helperStyle: GoogleFonts.poppins(color: Color(0xFF000000)),
-    errorStyle: GoogleFonts.poppins(color: Color(0xFFFF0000)),
-    disabledBorder: OutlineInputBorder(
-      borderSide: BorderSide(color: Color(0xFF808080)),
+    // Text, RichText, TextSpan
+    textTheme: TextTheme(
+      bodyLarge: GoogleFonts.poppins(color: Color(0xFF000000)),
+      bodyMedium: GoogleFonts.poppins(color: Color(0xFF000000)),
+      bodySmall: GoogleFonts.poppins(color: Color(0xFF000000)),
+      titleLarge: GoogleFonts.poppins(color: Color(0xFF000000)),
     ),
-    enabledBorder: OutlineInputBorder(
-      borderSide: BorderSide(color: Color(0xFF404040)),
+
+    // TextFormField, DropDownButton
+    inputDecorationTheme: InputDecorationTheme(
+      hintStyle: GoogleFonts.poppins(color: Color(0xFF404040)),
+      labelStyle: GoogleFonts.poppins(color: Color(0xFF000000)),
+      helperStyle: GoogleFonts.poppins(color: Color(0xFF000000)),
+      errorStyle: GoogleFonts.poppins(color: Color(0xFFFF0000)),
+      disabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color(0xFF808080)),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color(0xFF404040)),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color(0xFF000000)),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color(0xFFFF0000)),
+      ),
     ),
-    focusedBorder: OutlineInputBorder(
-      borderSide: BorderSide(color: Color(0xFF000000)),
+
+    // Selection TextFormField
+    textSelectionTheme: const TextSelectionThemeData(
+      cursorColor: Color(0xFF000000),
+      selectionColor: Color(0x40808080),
+      selectionHandleColor: Color(0xFF000000),
     ),
-    errorBorder: OutlineInputBorder(
-      borderSide: BorderSide(color: Color(0xFFFF0000)),
-    ),
-  ),
 
-  // Selection TextFormField
-  textSelectionTheme: const TextSelectionThemeData(
-    cursorColor: Color(0xFF000000),
-    selectionColor: Color(0x40808080),
-    selectionHandleColor: Color(0xFF000000),
-  ),
-
-  // CheckBox
-  checkboxTheme: CheckboxThemeData(
-    fillColor: WidgetStateProperty.resolveWith((states) {
-      if (states.contains(WidgetState.disabled)) return Color(0xFF808080);
-      if (states.contains(WidgetState.selected)) return Color(0xFF0080FF);
-      return Color(0x00000000);
-    }),
-    checkColor: WidgetStateProperty.all(Color(0xFFFFFFFF)),
-    overlayColor: WidgetStateProperty.all(Color(0xFF808080)),
-    side: BorderSide(color: Color(0xFF000000)),
-  ),
-
-  // Radio
-  radioTheme: RadioThemeData(
-    fillColor: WidgetStateProperty.resolveWith((states) {
-      if (states.contains(WidgetState.disabled)) return Color(0xFF808080);
-      if (states.contains(WidgetState.selected)) return Color(0xFF0080FF);
-      return Colors.red;
-    }),
-    overlayColor: WidgetStateProperty.all(Color(0xFF000000)),
-  ),
-
-  // Switch
-  switchTheme: SwitchThemeData(
-    thumbColor: WidgetStateProperty.resolveWith((states) {
-      if (states.contains(WidgetState.disabled)) return Color(0xFF808080);
-      if (states.contains(WidgetState.selected)) return Color(0xFF0080FF);
-      return Color(0xFFFFFFFF);
-    }),
-    trackColor: WidgetStateProperty.resolveWith((states) {
-      if (states.contains(WidgetState.disabled)) return Color(0xFF404040);
-      if (states.contains(WidgetState.selected)) return Color(0xFF8080FF);
-      return Color(0xFFBFBFBF);
-    }),
-    overlayColor: WidgetStateProperty.all(Color(0xFF000000)),
-    thumbIcon: WidgetStateProperty.resolveWith<Icon?>((states) {
-      Icon selectedIcon() {
-        return const Icon(Icons.check, size: 12, color: Color(0xFFFFFFFF));
-      }
-
-      Icon unselectedIcon() {
-        return const Icon(Icons.close, size: 12, color: Color(0xFF000000));
-      }
-
-      if (states.contains(WidgetState.selected)) return selectedIcon();
-      return unselectedIcon();
-    }),
-  ),
-
-  // DropDownMenu
-  dropdownMenuTheme: DropdownMenuThemeData(
-    textStyle: GoogleFonts.poppins(color: Color(0xFF000000)),
-    menuStyle: MenuStyle(
-      backgroundColor: WidgetStatePropertyAll(Color(0xFFFFFFFF)),
-      shadowColor: WidgetStatePropertyAll(Color(0x40000000)),
-    ),
-  ),
-
-  // ElevatedButton
-  elevatedButtonTheme: ElevatedButtonThemeData(
-    style: ButtonStyle(
-      backgroundColor: WidgetStateProperty.resolveWith((states) {
+    // CheckBox
+    checkboxTheme: CheckboxThemeData(
+      fillColor: WidgetStateProperty.resolveWith((states) {
         if (states.contains(WidgetState.disabled)) return Color(0xFF808080);
-        if (states.contains(WidgetState.pressed)) return Color(0xFF0080FF);
-        return Color(0xFF00BFFF);
+        if (states.contains(WidgetState.selected)) return MainColor.primary;
+        return Color(0x00000000);
       }),
-      overlayColor: WidgetStateProperty.all(Color(0xFF00BFFF)),
-      shadowColor: const WidgetStatePropertyAll(Color(0x40000000)),
+      checkColor: WidgetStateProperty.all(Color(0xFFFFFFFF)),
+      overlayColor: WidgetStateProperty.all(Color(0xFF808080)),
+      side: BorderSide(color: Color(0xFF000000)),
     ),
-  ),
 
-  // Bottom Sheet
-  bottomSheetTheme: const BottomSheetThemeData(
-    backgroundColor: Color(0xFFFFFFFF),
-  ),
-
-  // Dialog
-  dialogTheme: const DialogThemeData(backgroundColor: Color(0xFFFFFFFF)),
-
-  // Snack Bar
-  snackBarTheme: const SnackBarThemeData(backgroundColor: Color(0xFFFFFFFF)),
-
-  // App Bar
-  appBarTheme: const AppBarTheme(backgroundColor: Color(0xFF0080FF)),
-
-  // Floating Action Button
-  floatingActionButtonTheme: const FloatingActionButtonThemeData(
-    backgroundColor: Color(0xFF0080FF),
-    splashColor: Color(0xFF00BFFF),
-  ),
-
-  // Bottom Navigation Bar
-  bottomNavigationBarTheme: BottomNavigationBarThemeData(
-    backgroundColor: Color(0xFF0080FF),
-    selectedItemColor: Color(0xFFFFFFFF),
-    unselectedItemColor: Color(0xFFBFBFBF),
-    selectedIconTheme: IconThemeData(size: 20, color: Color(0xFFFFFFFF)),
-    unselectedIconTheme: IconThemeData(size: 16, color: Color(0xFFBFBFBF)),
-    selectedLabelStyle: GoogleFonts.poppins(
-      fontWeight: FontWeight.bold,
-      fontSize: 12,
+    // Radio
+    radioTheme: RadioThemeData(
+      fillColor: WidgetStateProperty.resolveWith((states) {
+        if (states.contains(WidgetState.disabled)) return Color(0xFF808080);
+        if (states.contains(WidgetState.selected)) return MainColor.primary;
+        return Colors.red;
+      }),
+      overlayColor: WidgetStateProperty.all(Color(0xFF000000)),
     ),
-    unselectedLabelStyle: GoogleFonts.poppins(
-      fontWeight: FontWeight.normal,
-      fontSize: 10,
+
+    // Switch
+    switchTheme: SwitchThemeData(
+      thumbColor: WidgetStateProperty.resolveWith((states) {
+        if (states.contains(WidgetState.disabled)) return Color(0xFF808080);
+        if (states.contains(WidgetState.selected)) return MainColor.primary;
+        return Color(0xFFFFFFFF);
+      }),
+      trackColor: WidgetStateProperty.resolveWith((states) {
+        if (states.contains(WidgetState.disabled)) return Color(0xFF404040);
+        if (states.contains(WidgetState.selected)) {
+          return MainColor.primary[400];
+        }
+        return Color(0xFFBFBFBF);
+      }),
+      overlayColor: WidgetStateProperty.all(Color(0xFF000000)),
+      thumbIcon: WidgetStateProperty.resolveWith<Icon?>((states) {
+        Icon selectedIcon() {
+          return const Icon(Icons.check, size: 12, color: Color(0xFFFFFFFF));
+        }
+
+        Icon unselectedIcon() {
+          return const Icon(Icons.close, size: 12, color: Color(0xFF000000));
+        }
+
+        if (states.contains(WidgetState.selected)) return selectedIcon();
+        return unselectedIcon();
+      }),
     ),
-  ),
-);
+
+    // DropDownMenu
+    dropdownMenuTheme: DropdownMenuThemeData(
+      textStyle: GoogleFonts.poppins(color: Color(0xFF000000)),
+      menuStyle: MenuStyle(
+        backgroundColor: WidgetStatePropertyAll(Color(0xFFFFFFFF)),
+        shadowColor: WidgetStatePropertyAll(Color(0x40000000)),
+      ),
+    ),
+
+    // ElevatedButton
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.resolveWith((states) {
+          if (states.contains(WidgetState.disabled)) return Color(0xFF808080);
+          if (states.contains(WidgetState.pressed)) return MainColor.primary;
+          return Color(0xFF00BFFF);
+        }),
+        overlayColor: WidgetStateProperty.all(Color(0xFF00BFFF)),
+        shadowColor: const WidgetStatePropertyAll(Color(0x40000000)),
+      ),
+    ),
+
+    // Bottom Sheet
+    bottomSheetTheme: const BottomSheetThemeData(
+      backgroundColor: Color(0xFFFFFFFF),
+    ),
+
+    // Dialog
+    dialogTheme: const DialogThemeData(backgroundColor: Color(0xFFFFFFFF)),
+
+    // Snack Bar
+    snackBarTheme: const SnackBarThemeData(backgroundColor: Color(0xFFFFFFFF)),
+
+    // App Bar
+    appBarTheme: AppBarTheme(backgroundColor: MainColor.primary),
+
+    // Floating Action Button
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: MainColor.primary,
+      splashColor: Color(0xFF00BFFF),
+    ),
+
+    // Bottom Navigation Bar
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: MainColor.primary,
+      selectedItemColor: Color(0xFFFFFFFF),
+      unselectedItemColor: Color(0xFFBFBFBF),
+      selectedIconTheme: IconThemeData(size: 20, color: Color(0xFFFFFFFF)),
+      unselectedIconTheme: IconThemeData(size: 16, color: Color(0xFFBFBFBF)),
+      selectedLabelStyle: GoogleFonts.poppins(
+        fontWeight: FontWeight.bold,
+        fontSize: 12,
+      ),
+      unselectedLabelStyle: GoogleFonts.poppins(
+        fontWeight: FontWeight.normal,
+        fontSize: 10,
+      ),
+    ),
+
+    progressIndicatorTheme: ProgressIndicatorThemeData(
+      color: MainColor.primary,
+      circularTrackColor: MainColor.primary[400],
+      linearTrackColor: MainColor.primary[400],
+    ),
+  );
+
+  ThemeData dark = ThemeData(
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: Color(0xFF404040),
+
+    // Text, RichText, TextSpan
+    textTheme: TextTheme(
+      bodyLarge: GoogleFonts.poppins(color: Color(0xFFFFFFFF)),
+      bodyMedium: GoogleFonts.poppins(color: Color(0xFFFFFFFF)),
+      bodySmall: GoogleFonts.poppins(color: Color(0xFFFFFFFF)),
+      titleLarge: GoogleFonts.poppins(color: Color(0xFFFFFFFF)),
+    ),
+
+    // TextFormField, DropDownButton
+    inputDecorationTheme: InputDecorationTheme(
+      hintStyle: GoogleFonts.poppins(color: Color(0xFFBFBFBF)),
+      labelStyle: GoogleFonts.poppins(color: Color(0xFFFFFFFF)),
+      helperStyle: GoogleFonts.poppins(color: Color(0xFFFFFFFF)),
+      errorStyle: GoogleFonts.poppins(color: Color(0xFFFF0000)),
+      disabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color(0xFF808080)),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color(0xFFBFBFBF)),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color(0xFFFFFFFF)),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color(0xFFFF0000)),
+      ),
+    ),
+
+    // Selection TextFormField
+    textSelectionTheme: const TextSelectionThemeData(
+      cursorColor: Color(0xFFFFFFFF),
+      selectionColor: Color(0x40808080),
+      selectionHandleColor: Color(0xFFFFFFFF),
+    ),
+
+    // CheckBox
+    checkboxTheme: CheckboxThemeData(
+      fillColor: WidgetStateProperty.resolveWith((states) {
+        if (states.contains(WidgetState.disabled)) return Color(0xFF808080);
+        if (states.contains(WidgetState.selected)) return MainColor.primary;
+        return Color(0x00000000);
+      }),
+      checkColor: WidgetStateProperty.all(Color(0xFF000000)),
+      overlayColor: WidgetStateProperty.all(Color(0xFF808080)),
+      side: BorderSide(color: Color(0xFFFFFFFF)),
+    ),
+
+    // Radio
+    radioTheme: RadioThemeData(
+      fillColor: WidgetStateProperty.resolveWith((states) {
+        if (states.contains(WidgetState.disabled)) return Color(0xFF808080);
+        if (states.contains(WidgetState.selected)) return MainColor.primary;
+        return Colors.red;
+      }),
+      overlayColor: WidgetStateProperty.all(Color(0xFF000000)),
+    ),
+
+    // Switch
+    switchTheme: SwitchThemeData(
+      thumbColor: WidgetStateProperty.resolveWith((states) {
+        if (states.contains(WidgetState.disabled)) return Color(0xFF808080);
+        if (states.contains(WidgetState.selected)) return MainColor.primary;
+        return Color(0xFF000000);
+      }),
+      trackColor: WidgetStateProperty.resolveWith((states) {
+        if (states.contains(WidgetState.disabled)) return Color(0xFFBFBFBF);
+        if (states.contains(WidgetState.selected)) {
+          return MainColor.primary[400];
+        }
+        return Color(0xFF404040);
+      }),
+      overlayColor: WidgetStateProperty.all(Color(0xFF000000)),
+      thumbIcon: WidgetStateProperty.resolveWith<Icon?>((states) {
+        Icon selectedIcon() {
+          return const Icon(Icons.check, size: 12, color: Color(0xFFFFFFFF));
+        }
+
+        Icon unselectedIcon() {
+          return const Icon(Icons.close, size: 12, color: Color(0xFFFFFFFF));
+        }
+
+        if (states.contains(WidgetState.selected)) return selectedIcon();
+        return unselectedIcon();
+      }),
+    ),
+
+    // DropDownMenu
+    dropdownMenuTheme: DropdownMenuThemeData(
+      textStyle: GoogleFonts.poppins(color: Color(0xFFFFFFFF)),
+      menuStyle: MenuStyle(
+        backgroundColor: WidgetStatePropertyAll(Color(0xFF404040)),
+        shadowColor: WidgetStatePropertyAll(Color(0x40000000)),
+      ),
+    ),
+
+    // ElevatedButton
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.resolveWith((states) {
+          if (states.contains(WidgetState.disabled)) return Color(0xFF808080);
+          if (states.contains(WidgetState.pressed)) return MainColor.primary;
+          return Color(0xFF00BFFF);
+        }),
+        overlayColor: WidgetStateProperty.all(Color(0xFF00BFFF)),
+        shadowColor: const WidgetStatePropertyAll(Color(0x40000000)),
+      ),
+    ),
+
+    // Bottom Sheet
+    bottomSheetTheme: const BottomSheetThemeData(
+      backgroundColor: Color(0xFF404040),
+    ),
+
+    // Dialog
+    dialogTheme: const DialogThemeData(backgroundColor: Color(0xFF000000)),
+
+    // Snack Bar
+    snackBarTheme: const SnackBarThemeData(backgroundColor: Color(0xFF000000)),
+
+    // App Bar
+    appBarTheme: AppBarTheme(backgroundColor: MainColor.primary),
+
+    // Floating Action Button
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: MainColor.primary,
+      splashColor: Color(0xFF00BFFF),
+    ),
+
+    // Bottom Navigation Bar
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: MainColor.primary,
+      selectedItemColor: Color(0xFFFFFFFF),
+      unselectedItemColor: Color(0xFFBFBFBF),
+      selectedIconTheme: IconThemeData(size: 20, color: Color(0xFFFFFFFF)),
+      unselectedIconTheme: IconThemeData(size: 16, color: Color(0xFFBFBFBF)),
+      selectedLabelStyle: GoogleFonts.poppins(
+        fontWeight: FontWeight.bold,
+        fontSize: 12,
+      ),
+      unselectedLabelStyle: GoogleFonts.poppins(
+        fontWeight: FontWeight.normal,
+        fontSize: 10,
+      ),
+    ),
+
+    progressIndicatorTheme: ProgressIndicatorThemeData(
+      color: MainColor.primary,
+      circularTrackColor: MainColor.primary[400],
+      linearTrackColor: MainColor.primary[400],
+    ),
+  );
+}
+
 
 /*
 | Opacity |   Hex  |
